cf349e9786a10676ee74510f2c98acb4352a82a4
guoxudong <guoxudong@keking.cn>
2019-11-20 16:22:55 +0800

    fix

--- dingtalk.go
+++ dingtalk.go
@@ -1,3 +1,4 @@
+package dingtalk
 /*
 Copyright © 2019 Guo Xudong
 
@@ -13,7 +14,6 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-package dingtalk
 
 import (
 	"bytes"
